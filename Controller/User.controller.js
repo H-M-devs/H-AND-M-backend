a112e9de8a79f65320d4c7dd90756a71228e9af5
khaled al shishani <khaled.shishani32@gmail.com>
2021-06-27 12:21:25 +0300

    day1

--- Controller/User.controller.js
+++ Controller/User.controller.js
@@ -1,4 +1,83 @@
-'use strict';
+"use strict";
 
-const {userModel, seedUserData} = require ('../Model/User.model');
+const { userModel, seedUserData } = require("../Model/User.model");
 
+const getdrugs = (req, res) => {
+  const { email } = req.query;
+
+  userModel.findOne({ email: email }, (error, user) => {
+    if (error) {
+      res.send(error);
+      //console.log(error)
+    } else {
+      res.json(user);
+      //console.log(user)
+    }
+  });
+};
+
+const createDruge = (request, response) => {
+   
+
+    console.log(request.body)
+    const { email, medicine } = request.body;
+
+    userModel.findOne({ email: email }, (error, userData) => {
+        if (error) {
+            response.send(error)
+        } else {
+            //userData.medicine.push(doctor);
+            userData.medicine.push(medicine);
+            userData.save();
+            response.json(userData);
+        }
+    })
+}
+
+const updateDrug = (request, response) => {
+    console.log(request.params)
+    const drugIndex = request.params.drug_idx;
+    const { email, medicine } = request.body;
+
+    userModel.findOne({ email: email }, (error, userData) => {
+        if (error) {
+            response.send(error)
+        } else {
+            userData.medicine.splice(drugIndex, 1, medicine);
+            userData.save();
+            response.send(userData)
+        }
+
+    });
+}
+
+
+
+const deleteDrug = (request, response) => {
+    console.log(request.params)
+    const drugIndex = request.params.drug_idx;
+    const { email } = request.query;
+
+    userModel.findOne({ email: email }, (error, userData) => {
+        if (error) {
+            response.send(error)
+        } else {
+            userData.books.splice(drugIndex,1);
+            userData.save();
+            response.send(userData)
+        }
+
+    });
+}
+
+
+
+
+module.exports ={ 
+    
+    getdrugs,
+    createDruge,
+    updateDrug,
+    deleteDrug
+
+};
--- Controller/base.controller.js
+++ Controller/base.controller.js
@@ -1,8 +1,11 @@
 'use strict';
 
+
+
 const base = (req, res)=> { // callback function of what we should do with our request
     res.send('Hello World') // our endpoint function response
   };
+
 
 
 
